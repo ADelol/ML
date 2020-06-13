34e81764af206f0fbbbc3d277dd298b6b01a02d1
ADelol <arnaud.delol@gmail.com>
2020-06-13 10:52:46 +0200

    aze

---
+++ TME4/sanstitre0.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 12 16:32:09 2020
+
+@author: arnau
+"""
+import numpy as np
+data = np.zeros((1000,2))
+a = np.zeros((2,2))
+print(a)
+print((data @ a.T).shape)
